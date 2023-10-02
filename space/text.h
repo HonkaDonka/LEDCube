2da2dcd3953b2930b98f65b28db1ff9ebb5bbe95
Vinson Huang <vinsonh789@gmail.com>
2023-10-01 19:49:17 -0500

    Prepare to add text

---
+++ space/text.h
@@ -0,0 +1,16 @@
+#ifndef FIREWORK_h
+#define FIREWORK_h
+
+#include "Arduino.h"
+#include "../power/graphics.h"
+#include "../gfx/chars.h"
+
+class Text : public Graphics
+{
+private:
+    
+public:
+    
+};
+
+#endif
